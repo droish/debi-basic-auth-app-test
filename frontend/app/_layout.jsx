6e2c7f940524d79b19aeb3b47e817d0f09341942
Matan <matanteaching@gmail.com>
2025-05-24 09:26:35 +0300

    Remove unnceessary.

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -11,7 +11,6 @@ const tabs = [
 export default function RootLayout() {
   return (
     <SafeAreaView className="flex-1 bg-white">
-      {/* Top navigation bar with margin from the top */}
       <View className="mt-8 h-16 flex-row justify-around items-center border-b border-gray-200 bg-white">
         {tabs.map(tab => (
           <Link key={tab.href} href={tab.href} asChild>
